b9d8426483d7eda54d95622de0a3b4b59eb0d846
yashkataraei <yashkataraei@gmail.com>
2025-08-05 16:11:36 +0530

    angular-v19

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-showname() {
-  alert("Hello, Yash Katara this side");
-  this.showname1();
-  console.log("Button clicked!");
-}
-showname1() {
-  alert("Hello, Yash Katara this side");
-  console.log("Button clicked!");
-}
+  task="";
+  taskList:{id:number, task:string} []= [];
+// showname() {
+//   alert("Hello, Yash Katara this side");
+//   this.showname1();
+//   console.log("Button clicked!");
+// }
+// showname1() {
+//   alert("Hello, Yash Katara this side");
+//   console.log("Button clicked!");
+// }
+
+  Addtask() {
+    this.taskList.push({id: this.taskList.length + 1, task: this.task});
+    this.task = "";
+    console.log(this.taskList);
+    alert("Task added successfully!");
+
+  }
+  DeleteTask() {
+    this.taskList.pop();
+    alert("Task deleted successfully!");
+    console.log(this.taskList);
+  }
 }
